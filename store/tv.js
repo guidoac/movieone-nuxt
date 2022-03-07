d4b7e01cdcf223462f48021df29fd32207e216fc
Guilherme Almada <guido.ptr13@gmail.com>
2022-03-07 14:07:57 -0300

    feat(modules): created tv details module vuex

---
+++ store/tv.js
@@ -0,0 +1,23 @@
+export const state = () => ({
+    current_details: {}
+})
+
+export const mutations = {
+    'SET_DETAILS' (state, data) {
+        state.current_details = data;
+    }
+}
+
+export const getters = {
+    current_tv_details: (state) => {
+        return state.current_details;
+    },
+}
+
+export const actions = {
+    async fetchDetails ({ commit }, params) {
+        const res = await this.$axios.$get(`/tv/${params.id}`);
+
+        commit('SET_DETAILS', res);
+    }
+}
