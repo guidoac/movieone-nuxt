bc2b0a02c76a8a4bf84a6ae5c5562d6eab3a734a
Guilherme Almada <guido.ptr13@gmail.com>
2022-05-12 16:52:15 -0300

    feat(filters): created date filter

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
+
 import _ from 'lodash';
+import { format } from 'date-fns';
 
 Vue.filter('capitalize', (value) => {
     const result = _.capitalize(value);
@@ -11,4 +13,12 @@ Vue.filter('price', (value) => {
     const result = `U$ ${value}`;
 
     return result;
+})
+
+Vue.filter('date', value => {
+    if (value) {
+        const date = format(new Date(value), 'MMM d, yyyy');
+    
+        return date;
+    }
 })
