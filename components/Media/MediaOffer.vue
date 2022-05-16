25c06c3cb3d759b37ec9720b99134a7df35025a3
Guilherme Almada <guido.ptr13@gmail.com>
2022-05-16 18:41:19 -0300

    feat(media-offer): added star rating to media offer component

--- components/Media/MediaOffer.vue
+++ components/Media/MediaOffer.vue
@@ -1,7 +1,37 @@
 <template>
     <div>
         <MediaBox>
-            <MediaCoverPhoto :url="media.poster_path" />
+            <MediaCoverPhoto
+                :url="media.poster_path"
+                class="col-8 mb-2"
+            />
+
+            <p class="bg-primary bg-opacity-25 p-2 rounded-2xl border border-primary h8 text-primary text-uppercase">
+                {{ $getGenre }}
+            </p>
+
+            <div class="row w-100 justify-content-between">
+                <div class="col-6">
+                    <StarRating
+                        :star-size="20"
+                        :rating="media.vote_average"
+                        :read-only="true"
+                        active-color="#fcbc04"
+                    />
+
+                    <p class="text-muted mt-2 h8">{{ `${media.vote_count} Reviews` }}</p>
+                </div>
+
+                <div class="col-6 text-end">
+                    <p class="fw-bold fs-5 text-light mb-0">
+                        {{ randomPrice | price }} 
+                    </p>
+
+                    <p class="h8 text-decoration-line-through text-muted">
+                       {{ randomPrice + 20 | price }} 
+                    </p>
+                </div>
+            </div>
         </MediaBox>
     </div>
 </template>
@@ -10,21 +40,27 @@
 import MediaBox from '~/components/MediaBox';
 import MediaCoverPhoto from '~/components/MediaCoverPhoto';
 
-    export default {
-        name: 'MediaOffer',
+export default {
+    name: 'MediaOffer',
 
-        components: {
-            MediaBox,
-            MediaCoverPhoto
-        },
+    components: {
+        MediaBox,
+        MediaCoverPhoto,
+    },
 
-        props: {
-            media: {
-                type: Object,
-                required: true
-            }
+    props: {
+        media: {
+            type: Object,
+            required: true
         }
-    }
+    },
+
+    computed: {
+        randomPrice () {
+            return Number(this.$utils.random(1, 100, true).toFixed(2));
+        }
+    },
+}
 </script>
 
 <style lang="scss" scoped>
--- plugins/components.js
+++ plugins/components.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import VueSlickCarousel from 'vue-slick-carousel';
+import StarRating from 'vue-star-rating';
 
 Vue.component(VueSlickCarousel);
+Vue.component('StarRating', StarRating);
